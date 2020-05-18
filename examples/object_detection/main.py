88e953b89f7854607d79e27b8c74c1276e7f3e64
thomas-alectio <65536813+thomas-alectio@users.noreply.github.com>
2020-05-18 15:35:54 -0700

    Modified main file to correct name

--- examples/object_detection/main.py
+++ examples/object_detection/main.py
@@ -2,7 +2,7 @@ from alectio_sdk.flask_wrapper import Pipeline
 from processes import train,test,infer
 
 # put the train/test/infer processes into the constructor
-app = Pipeline(name='daily_dialog', train_fn=train, 
+app = Pipeline(name='coco', train_fn=train, 
                test_fn=test, infer_fn=infer)
 
 
