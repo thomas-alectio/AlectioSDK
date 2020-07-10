IMDB Demo + fix pre_softmax computation in other demos

examples/mushroom_classification/processes.py
@@ -11,6 +11,9 @@ import torch.optim as optim
 import os
 import yaml
 import argparse
+import torch.nn.functional as F
+from scipy.special import logit as logit_fn
+from scipy.special import expit
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -178,10 +181,14 @@ def infer(args, unlabeled, ckpt_file):
             for logit in prediction:
                 predictions[unlabeled[predix]] = {}
                 prediction = 0
+
+                #since I include sigmoid as the last layer's activation function, logit = %
                 if logit.cpu().numpy() > 0.5:
                     prediction = 1
                 predictions[unlabeled[predix]]["prediction"] = prediction
-                predictions[unlabeled[predix]]["pre_softmax"] = [logit.cpu().numpy()[0], 1-logit.cpu().numpy()[0]]
+
+                predictions[unlabeled[predix]]["pre_softmax"] = [logit_fn(logit.cpu().numpy()[0]), logit_fn(1 - logit.cpu().numpy()[0])]
+                
 
                 predix+=1
             